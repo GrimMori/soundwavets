4798520d584f2365cdf8b3817a406aa36e95a5df
mori <armigofire@gmail.com>
2023-02-08 00:52:49 +0100

    color scheme

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+export const colors = {
+    "general-bg": "#2F303A",
+    "nav-bg": "#202027",
+    "nav-hover": "#909097",
+    "border-input": "#2E3852",
+    "border-input-focus": "#596da0",
+    "button-bg": "#1762A7",
+    "button-hover": "#2180d8",
+    "button-focus": "#270000",
+    "accent-text": "#ff2299",
+    "circle-1": "#bc3a801a",
+    "circle-2": "#3457b21a"
+}
